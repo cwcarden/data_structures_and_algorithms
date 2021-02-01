b0d93082fb31d753bd4e6d8da41408a5b188f33c
charles <charles@charlescarden.com>
2021-01-31 19:21:27 -0600

    completed linear search for loop

--- Chapter 1 Abstraction and Analysis/01_programming_exercises.py
+++ Chapter 1 Abstraction and Analysis/01_programming_exercises.py
@@ -4,6 +4,23 @@ time.time function as we did in the examples in this chapter) the worst and
 best cases for the list index method version of the linear search, the linear
 search code written using a for statement, and the binary search code.  In the
 comments list the specifications of your computer (CPU chip and clock speed,
-commentsoperating system, and Python version) along with the worst and best
+operating system, and Python version) along with the worst and best
 times for each of the three searches.
 """
+import time
+
+
+def linear_search_speed(target):
+    items = range(1_000_000)
+    start = time.time()
+    for item in items:
+        try:
+            my_index = item.index(target)
+        except:
+            ValueError
+    stop = time.time()
+    print(f"{target} integer executed in {stop - start} seconds")
+
+linear_search_speed(999999)
+linear_search_speed(499999)
+linear_search_speed(10)
